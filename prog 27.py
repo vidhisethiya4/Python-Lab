9e948999445994a6c8e8b86713266da3c8f02ace
vidhisethiya4 <76218307+vidhisethiya4@users.noreply.github.com>
2021-10-09 16:36:57 +0530

    Add files via upload

---
+++ prog 27.py
@@ -0,0 +1,14 @@
+
+rows = int(input(" Enter the no of rows: ")) 
+      
+k = rows - 1
+for i in range(0, rows):
+    
+    for j in range(0, k):
+        print(end=" ")
+    k = k - 1
+     
+    for j in range(0, i+1):
+        print(" * ", end="")
+     
+    print("\r")
