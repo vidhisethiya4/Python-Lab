42dfec59a34be56b0fcbb8a7f9106751120e6a6d
vidhisethiya4 <76218307+vidhisethiya4@users.noreply.github.com>
2021-10-09 23:54:00 +0530

    Add files via upload

---
+++ prog 22.py
@@ -0,0 +1,12 @@
+print(end="Enter a Number: ")
+num = int(input())
+print("\nMultiplication Table of " +str(num))
+i = 1
+while i<11:
+  print(str(num)+ " X " +str(i)+ " = " +str(num*i))
+  i = i+1
+
+
+
+
+
